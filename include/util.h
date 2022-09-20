03944541a971063253dfe59677fa46cb8e098bbf
Chloekkk <skdusdl8804@gmail.com>
2022-09-20 16:27:51 +0900

    feat: Add ft_str_to_rgb.c

--- include/util.h
+++ include/util.h
@@ -1,6 +1,12 @@
 #ifndef UTIL_H
-#define UTIL_H
+# define UTIL_H
+
+# include "cub3D.h"
 
 char	*ft_get_line(int fd);
+char	*ft_trim_line(char *line);
+
+void	*ft_make_img(void *mlx_ptr, char *xpmFile);
+int		ft_str_to_rgb(char *str);
 
 #endif
