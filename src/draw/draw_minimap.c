0fe3b0c3c449ed47417e1c0073d9e98c880fff8c
Chloekkk <skdusdl8804@gmail.com>
2022-09-24 12:56:58 +0900

    feat: Add minimap files

---
+++ src/draw/draw_minimap.c
@@ -0,0 +1,48 @@
+#include "cub3D.h"
+#include "util.h"
+
+void	draw_minimap_player(t_game *game, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			mlx_pixel_put(game->gl.mlx_ptr, game->gl.win_ptr,
+					game->minimap_info.pos.x + i, game->minimap_info.pos.y + j,
+					0xffff00);
+		}
+	}
+}
+
+void	draw_minimap_bg(t_game *game)
+{
+	t_map_info map_info;
+	char *line;
+	int row;
+	int col;
+
+	map_info = game->map_info;
+	row = 0;
+	while (row < map_info.height)
+	{
+		line = map_info.map[row];
+		col = 0;
+		while (line[col] && col < map_info.width)
+		{
+			if (line[col] == '1')
+				ft_put_img(&game->gl, game->minimap_info.wall, col * BLOCK_SIZE,
+						row * BLOCK_SIZE);
+			else
+				ft_put_img(&game->gl, game->minimap_info.space, col
+						* BLOCK_SIZE, row * BLOCK_SIZE);
+			col++;
+		}
+		while (col < map_info.width)
+		{
+			ft_put_img(&game->gl, game->minimap_info.space, col * BLOCK_SIZE,
+					row * BLOCK_SIZE);
+			col++;
+		}
+		row++;
+	}
+}
