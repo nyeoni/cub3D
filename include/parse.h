b2f5c773d481ed769b7bfb1b6dcec79d65d1120e
Kimhannah <skqkdldhf98@naver.com>
2022-09-29 11:55:46 +0900

    refactor Modify static set state functions to external function

--- include/parse.h
+++ include/parse.h
@@ -9,6 +9,10 @@ void	parse_map_info(t_map_info *map_info, int fd);
 void	parse_graphic_info(t_graphic_info *graphic_info, int fd, void *mlx_ptr);
 void	parse_state(t_state *state, t_map_info *map_info);
 
+void	set_dir(t_state *state, double dir_x, double dir_y);
+void	set_plane(t_state *state);
+void	set_pos(t_state *state, double x, double y);
+
 void	valid_map(t_game *game);
 void	valid_edge_wall(t_map_info *map_info, int row);
 void	valid_inner_wall(t_map_info *map, int row);
