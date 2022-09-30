6a439a2477f994c3a51c1ba032f3fadf0eb95784
Chloekkk <skdusdl8804@gmail.com>
2022-09-30 16:14:21 +0900

    fix: Fix edge wall error

--- src/parse/valid_map.c
+++ src/parse/valid_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   valid_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hannkim <hannkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: nkim <nkim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:49:23 by nkim              #+#    #+#             */
-/*   Updated: 2022/09/30 12:59:43 by hannkim          ###   ########.fr       */
+/*   Updated: 2022/09/30 16:19:00 by nkim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,54 @@ static void	count_player(char *line, int *player_cnt)
 	}
 }
 
-void	valid_map(t_game *game)
+static char ** map_dup(t_map_info *map_info)
 {
-	t_map_info	*map_info;
+	int row;
+	int col;
+	int size;
+	char **map;
+
+	map = ft_calloc(map_info->height, sizeof(char *));
+	if (!map)
+		throw_error("Error : malloc error.");
+	row = -1;
+	while (++row < map_info->height) {
+		map[row] = ft_calloc(map_info->width + 1, sizeof(char));
+		col = -1;
+		size = ft_strlen(map_info->map[row]);
+		while (++col < size)
+			map[row][col] = map_info->map[row][col];
+		while (col < map_info->width)
+		{
+			map[row][col] = ' ';
+			col++;
+		}
+	}
+	return (map);
+}
+
+void	valid_map(t_map_info *map_info)
+{
+	char		**map;
 	char		*line;
 	int			row;
 	int			player_cnt;
 
-	map_info = &game->map_info;
 	row = 0;
 	player_cnt = 0;
+	map = map_dup(map_info);
 	while (row < map_info->height)
 	{
-		line = map_info->map[row];
+		line = map[row];
 		count_player(line, &player_cnt);
 		valid_edge_wall(line, map_info->height, row);
-		valid_inner_wall(line, map_info->map, map_info->height, row);
+		valid_inner_wall(line, map, map_info->height, row);
 		row++;
 	}
 	if (player_cnt != 1)
 		throw_error("InvalidMapError : player must exist only one");
+	row = -1;
+	while (++row < map_info->height)
+		free(map[row]);
+	free(map);
 }
